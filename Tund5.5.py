f7c7e797f7d5eb454763d638ff4f0d340f084c6a
denchikLOG <deniss.melnikov@gmail.com>
2025-05-16 15:43:16 +0300

    Update Tund5.5.py

--- Tund5.5.py
+++ Tund5.5.py
@@ -35,3 +35,69 @@ def töötajad():
             print("Nägemist!")
             break
 töötajad()
+
+####
+def töötajad():
+    nimed = []
+    sünniajad = []
+    n = int(input("Mitu töötajat küsitleda? "))
+    for _ in range(n):
+        nimi = input("Töötaja nimi: ")
+        aasta = int(input(f"{nimi} sünniaasta: "))
+        nimed.append(nimi)
+        sünniajad.append(aasta)
+
+    def vanus(aasta):
+        return 2025 - aasta
+
+    while True:
+        print("\n1 - Pensionärid\n2 - Keskmine vanus\n3 - 10 nooremat/vanemat\n4 - Otsi sünniaasta järgi\n0 - Välju")
+        valik = input("Valik: ")
+
+        if valik == "1":
+            for nimi, aasta in zip(nimed, sünniajad):
+                if vanus(aasta) >= 65:
+                    print(f"{nimi} - {vanus(aasta)} aastat")
+
+        elif valik == "2":
+            koguvanus = 0
+            for aasta in sünniajad:
+                koguvanus += vanus(aasta)
+            keskmine = koguvanus / len(sünniajad)
+            print(f"Keskmine vanus: {keskmine:.2f} aastat")
+
+        elif valik == "3":
+            valik2 = int(input("1 - Nooremat, 2 - Vanemat: "))
+            andmed = list(zip(nimed, sünniajad))
+
+            # Tavaline sorteerimine ilma lambda'ta
+            for i in range(len(andmed)):
+                for j in range(i + 1, len(andmed)):
+                    vanus1 = vanus(andmed[i][1])
+                    vanus2 = vanus(andmed[j][1])
+                    if (valik2 == 1 and vanus1 > vanus2) or (valik2 == 2 and vanus1 < vanus2):
+                        andmed[i], andmed[j] = andmed[j], andmed[i]
+
+            print("Tulemused:")
+            for i in range(min(10, len(andmed))):
+                nimi, aasta = andmed[i]
+                print(f"{i + 1}. {nimi} - {vanus(aasta)} aastat")
+
+        elif valik == "4":
+            otsitav = int(input("Sisesta sünniaasta: "))
+            leitud = False
+            for nimi, aasta in zip(nimed, sünniajad):
+                if aasta == otsitav:
+                    print(f"{nimi} - {aasta}")
+                    leitud = True
+            if not leitud:
+                print("Sellise sünniaastaga töötajat ei leitud!")
+
+        elif valik == "0":
+            print("Nägemist!")
+            break
+
+        else:
+            print("Tundmatu valik, proovi uuesti.")
+
+töötajad()
